121c9bd6505fbe289173a9e5d61ee38b8ba560a0
Jennifer Cen <cenjennifer@gmail.com>
2016-08-26 13:15:40 -0400

    completed module-2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Main } from './containers';
 
 @Component({
   selector: 'app',
+  directives: [
+  	Main
+  ],
   template: `
     <div>
       <h3>
         Yo, world!
       </h3>
+      <main-container></main-container>
     </div>
   `
 })
