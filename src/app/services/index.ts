0c25cab97490fcd8042ef8310527975d64664052
Jennifer Cen <cenjennifer@gmail.com>
2016-08-28 14:55:11 -0400

    completed module-10

---
+++ src/app/services/index.ts
@@ -0,0 +1,8 @@
+export { ApiService } from './api';
+import * as services from './services';
+
+const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
+
+export const providers = [
+	...mapValuesToArray(services)
+];
